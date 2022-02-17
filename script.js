c4d4d7e8e5a3d092a76feb3c45539ec2d02ddf53
manoj <manojreddy.a2@gmail.com>
2022-02-17 17:23:26 +0530

    edited script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
 
 let score = 20;
 
@@ -11,6 +10,7 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.message').textContent = 'No Number!';
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem'
     } else if (guess > secretNumber) {
